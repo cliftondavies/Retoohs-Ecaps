e7b2f49f4612340df17e3d0fd5981109ecb08ac3
cliftondavies <clifton.davies@outlook.com>
2021-01-25 22:46:40 +0000

    Clean up preloader.js

--- src/scenes/preloader.js
+++ src/scenes/preloader.js
@@ -4,26 +4,12 @@ import 'phaser';
 class Preloader extends Phaser.Scene {
   constructor() {
     super('Preloader');
-    // this.readyCount = 0;
   }
-
-  // init() {
-
-  // }
-
-  // ready() {
-  //   this.readyCount += 1;
-  //   if (this.readyCount === 2) {
-  //     this.scene.stop('Preloader');
-  //     this.scene.start('Title');
-  //   }
-  // }
 
   preload() {
     this.add.image(400, 300, 'background');
     this.add.image(400, 300, 'logo');
 
-    // display progress bar
     const progressBar = this.add.graphics();
     const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
@@ -63,7 +49,6 @@ class Preloader extends Phaser.Scene {
     });
     assetText.setOrigin(0.5, 0.5);
 
-    // update progress bar
     this.load.on('progress', (value) => {
       percentText.setText(`${parseInt(value * 100, 10)}%`);
       progressBar.clear();
@@ -71,22 +56,17 @@ class Preloader extends Phaser.Scene {
       progressBar.fillRect(250, 280, 300 * value, 30);
     });
 
-    // update file progress text
     this.load.on('fileprogress', (file) => {
       assetText.setText(`Loading asset: ${file.key}`);
     });
 
-    // remove progress bar when complete
     this.load.on('complete', () => {
       progressBar.destroy();
       progressBox.destroy();
       loadingText.destroy();
       percentText.destroy();
       assetText.destroy();
-      // this.ready();
     });
-
-    // this.time.delayedCall(1000, this.ready, [], this); // remove timeevent
 
     this.load.image('player', 'assets/starship.svg');
     this.load.image('enemy', 'assets/starshipdark.svg');
