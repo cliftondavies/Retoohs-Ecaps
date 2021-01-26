c1320abc07667506c3f1f5dc1217e5ecd75f9b3f
cliftondavies <clifton.davies@outlook.com>
2021-01-26 12:08:53 +0000

    Update leasderboard scene

--- src/scenes/leaderboard.js
+++ src/scenes/leaderboard.js
@@ -21,14 +21,14 @@ class Leaderboard extends Phaser.Scene {
 
   displayLeaderboard(topTen, currentScore) {
     const leaderboardText = [];
-    let y = 60;
+    let y = 65;
     topTen.forEach((score, index) => {
       if (score === currentScore) {
-        const text = this.add.text(10, y, `Your Score: ${'.'.repeat(84)} ${score}`, { fontSize: '15px', fill: '#ffffff' });
+        const text = this.add.text(25, y, `Your Score: ${'.'.repeat(128)} ${score}`, { fontSize: '15px', fill: '#ffffff' });
         leaderboardText.push(text);
       } else {
-        const seperator = (index < 9) ? '.'.repeat(99) : '.'.repeat(97);
-        const text = this.add.text(10, y, `${index + 1}: ${seperator} ${score}`, { fontSize: '15px', fill: '#ffffff' });
+        const seperator = (index < 9) ? '.'.repeat(143) : '.'.repeat(141);
+        const text = this.add.text(25, y, `${index + 1}: ${seperator} ${score}`, { fontSize: '15px', fill: '#ffffff' });
         leaderboardText.push(text);
       }
       y += 50;
@@ -39,7 +39,7 @@ class Leaderboard extends Phaser.Scene {
   async create() {
     try {
       this.add.image(400, 300, 'background');
-      this.add.text(290, 10, 'Top 10 Leaderboard', { fontSize: '25px', fill: '#ffffff' });
+      this.add.text(290, 25, 'Top 10 Leaderboard', { fontSize: '25px', fill: '#ffffff' });
       this.add.text(310, 550, 'Click to play again', { fontSize: '20px', fill: '#ffffff' });
       const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ifdS41s0Fmwd6vzf41Kt/scores/';
       const scoresJson = await fetch(apiUrl);
@@ -55,7 +55,7 @@ class Leaderboard extends Phaser.Scene {
       });
       return leaderboardText;
     } catch (error) {
-      const errorText = this.add.text(570, 10, `${error}`, { fontSize: '15px', fill: '#ff0000' });
+      const errorText = this.add.text(10, 560, `${error}`, { fontSize: '15px', fill: '#ff0000' });
       return errorText;
     }
   }
